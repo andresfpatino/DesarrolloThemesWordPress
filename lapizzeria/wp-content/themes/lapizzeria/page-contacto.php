13d24106046020da2433214640f9a58223271678
Andrés F. Patiño <andresfelipepatino5@gmail.com>
2018-10-20 21:05:51 -0500

    Página de contacto

---
+++ lapizzeria/wp-content/themes/lapizzeria/page-contacto.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+* Template Name: Contacto
+*/
+
+ get_header(); ?>  
+
+    <?php while(have_posts()): the_post(); ?> 
+        <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
+            <div class="contenido-hero">
+                <div class="text-hero">
+                    <h1><?php the_title(); ?> </h1>
+                    <div class="breadcrumb">                   
+                        <?php 
+                            if(function_exists('bcn_display') && !is_page(2)){
+                                bcn_display();
+                            }
+                        ?>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="principal contenedor contacto">
+            <main class="contenido-paginas">               
+                <form class="reserva-contacto" method="POST">
+                     <h2>Realiza una reservación</h2>
+                    <div class="campo">
+                        <input type="text" name="nombre" placeholder="Nombre" required>
+                    </div>
+                    <div class="campo">
+                        <input type="datetime-local" name="fecha" placeholder="Fecha" required>                        
+                    </div>
+                    <div class="campo">
+                        <input type="emai" name="correo" placeholder="Correo" required>
+                    </div>
+                    <div class="campo">
+                        <input type="tel" name="telefono" placeholder="Teléfono" required>
+                    </div>
+                    <div class="campo">
+                        <textarea name="menasje" placeholder="Mensaje" required></textarea>
+                    </div>
+                    <input type="submit" name="enviar" class="button">
+                </form>
+            </main>
+        </div>
+    <?php endwhile; ?>
+    
+<?php get_footer(); ?>
